dad18294b2d157d7fdda3b45f092b2f72bc3f90d
sadisticblooper <laddygodden0@gmail.com>
2025-07-22 20:55:49 +0530

    Update data.js

--- data.js
+++ data.js
@@ -30,7 +30,7 @@ const modData = {
         },
         {
           name: "Ironclad challange stance",
-          image: "ironclad challange.webp",
+          image: "assets/ironclad challange.webp",
           link: "https://t.me/idkjetd/49"
         }
       ]

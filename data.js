0cb8c32a556908342d26faca84aa0acc7aa52b06
sadisticblooper <laddygodden0@gmail.com>
2025-07-23 22:39:35 +0530

    Update data.js

--- data.js
+++ data.js
@@ -4,39 +4,39 @@ const modData = {
       name: "Stance",
       subbundles: [
         {
-          name: "Itu apple cutting stance for iaodo Katana",
+          name: "Ioado katana apple cutting stance",
           image: "assets/dote apple.webp",
-          link: "https://t.me/idkjetd/3"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/Ioado%20katana%20stance%20apple%20cutting%20.zip"
         },
         {
           name: "Glitch Markus for Giant sword",
           image: "assets/Markus giant sword.webp",
-          link: "https://t.me/idkjetd/5"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/Glitch%20Markus%20stance.zip"
         },
         {
           name: "Kissing opponent for good luck stance for knuckles",
           image: "assets/kiss.webp",
-          link: "https://t.me/idkjetd/23"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/Kissing%20opponent%20for%20good%20luck%20stance%20for%20knuckles%20.zip"
         },
         {
           name: "katana qigong stance",
           image: "assets/qigong.webp",
-          link: "https://t.me/idkjetd/25"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/Qigong%20stance%20katana%20.zip"
         },
         {
           name: "Helga stance for single handed Sword",
           image: "assets/HelgaOnehandsword.webp",
-          link: "https://t.me/idkjetd/47"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/Helga%20stance%20for%20single%20handed%20sword.zip"
         },
         {
           name: "Ironclad challange stance",
           image: "assets/ironclad challenge.webp",
-          link: "https://t.me/idkjetd/49"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/Ironclad%20challenge%20stance.zip"
         },
         {
           name: "Spinning Naginata",
           image: "assets/Spinning Naginata.webp",
-          link: "https://t.me/idkjetd/53"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Stances/spinning%20nagainata.zip"
         }
       ]
     },
@@ -46,27 +46,27 @@ const modData = {
         {
           name: "Dote recolour indigo theme",
           image: "assets/dote indigo skin.webp",
-          link: "https://t.me/idkjetd/11"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Skin%20mod/Dote%20recolour%20indigo%20theme.zip"
         },
         {
           name: "Glitch Markus",
           image: "assets/glitch markus skin.webp",
-          link: "https://t.me/idkjetd/13"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Skin%20mod/glitch%20markus%20texture%20mod.zip"
         },
         { 
           name: "void warden becomes shadow mind",
           image: "assets/void×shadowmind skin.webp",
-          link: "https://t.me/idkjetd/15"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Skin%20mod/Void%20warden%20becomes%20shadow%20mind.zip"
         },
         {
           name: "Librator Purple theme mod",
           image: "assets/libratorpurple.webp",
-          link: "https://t.me/idkjetd/31"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Skin%20mod/Liberator%20Recolor.zip"
         },
         {
           name: "Biblically accurate black shadow artisan",
           image: "assets/shadow artisan.webp",
-          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/okada-set-superblack-1.0.zip"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Skin%20mod/okada-set-superblack-1.0.zip"
         }
       ]
     }
@@ -77,21 +77,29 @@ const modData = {
       subbundles: [
         {
           name: "Dual Swords stance",
-          link: "https://t.me/idkjetd/39"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Weapons/Dual%20sword%20stance%20(not%20modded%20.zip"
         },
         {
           name: "Composite Sword stance",
-          link: "https://t.me/idkjetd/41"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Weapons/Composite%20sword%20stance%20(not%20modded).zip"
           
         },
         {
           name: "Giant Sword Stance",
-          link: "https://t.me/idkjetd/43"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Weapons/Giant%20sword%20stance(not%20modded).zip"
         },
         {
           name: "Two Handed Sword Stance",
-          link: "https://t.me/idkjetd/45"
-        }
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Weapons/Two%20handed%20sword%20stance%20(not%20modded).zip"
+        },
+        {
+          name: "Naginata Stance",
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Weapons/Naginata%20stance%20(not%20modded).zip"
+        },
+        {
+          name: "Single handed Sword stance",
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Weapons/Single%20handed%20sword%20stances(not%20modded).zip"
+            
       ]
     },
     {
@@ -99,15 +107,15 @@ const modData = {
       subbundles: [
         {
           name: "Shadow's moveset",
-          link: "https://t.me/idkjetd/21"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Moveset%20and%20miscellaneous/Shadow's%20moveset%20(not%20modded).zip"
         },
         {
           name: "Bolo stance and animation",
-          link: "https://t.me/idkjetd/33"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Moveset%20and%20miscellaneous/Bolo%20animation%20(not%20modded).zip"
         },
         {
           name: "Stranger animations",
-          link: "https://t.me/idkjetd/35"
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Moveset%20and%20miscellaneous/Stranger%20animations(not%20modded).zip"
         }
       ]
     },
@@ -115,8 +123,8 @@ const modData = {
       name: "Sound Assets",
       subbundles: [
         {
-          name: "Block Sounds",
-          link: "https://t.me/idkjetd/37"
+          name: "Some Block Sounds",
+          link: "https://github.com/sadisticblooper/SF3MODHUB/raw/refs/heads/main/Modfiles/Raw%20material/Sounds/Some%20weapon%20block%20sounds(not%20modded).zip"
         }
       ]
     }
